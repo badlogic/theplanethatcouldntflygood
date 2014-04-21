2a1f1160cc81dfae947610b1fb4c130c12c7fae0
badlogic <contact@badlogicgames.com>
2014-04-21 20:03:10 +0200

    added rocks

--- core/src/com/badlogicgames/plane/PlaneGame.java
+++ core/src/com/badlogicgames/plane/PlaneGame.java
@@ -10,7 +10,9 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
 
 public class PlaneGame extends ApplicationAdapter {
 	private static final float PLANE_JUMP_IMPULSE = 350;
@@ -21,15 +23,18 @@ public class PlaneGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	OrthographicCamera camera;
 	Texture background;
-	Texture ground;
+	TextureRegion ground;
 	float groundOffsetX = 0;
 	TextureRegion ceiling;
+	TextureRegion rock;
+	TextureRegion rockDown;
 	Animation plane;
 	
 	Vector2 planePosition = new Vector2();
 	Vector2 planeVelocity = new Vector2();
 	float planeStateTime = 0;
 	Vector2 gravity = new Vector2();
+	Array<Rock> rocks = new Array<Rock>();
 	
 	@Override
 	public void create () {
@@ -38,9 +43,13 @@ public class PlaneGame extends ApplicationAdapter {
 		camera.setToOrtho(false, 800, 480);
 		
 		background = new Texture("background.png");	
-		ground = new Texture("ground.png");
+		ground = new TextureRegion(new Texture("ground.png"));
 		ceiling = new TextureRegion(ground);
 		ceiling.flip(true, true);
+		
+		rock = new TextureRegion(new Texture("rock.png"));
+		rockDown = new TextureRegion(rock);
+		rockDown.flip(false, true);
 		
 		Texture frame1 = new Texture("plane1.png");
 		frame1.setFilter(TextureFilter.Linear, TextureFilter.Linear);
@@ -52,6 +61,11 @@ public class PlaneGame extends ApplicationAdapter {
 		planePosition.set(PLANE_START_X, PLANE_START_Y);
 		planeVelocity.set(PLANE_VELOCITY_X, 0);
 		gravity.set(0, GRAVITY);
+		
+		for(int i = 0; i < 5; i++) {
+			boolean isDown = MathUtils.randomBoolean();
+			rocks.add(new Rock(700 + i * 200, isDown?480-rock.getRegionHeight(): 0, isDown? rockDown: rock));
+		}
 	}
 	
 	private void updateWorld() {
@@ -66,8 +80,17 @@ public class PlaneGame extends ApplicationAdapter {
 		planePosition.mulAdd(planeVelocity, deltaTime);
 		
 		camera.position.x = planePosition.x + 350;		
-		if(camera.position.x - groundOffsetX > ground.getWidth() + 400) {
-			groundOffsetX += ground.getWidth();
+		if(camera.position.x - groundOffsetX > ground.getRegionWidth() + 400) {
+			groundOffsetX += ground.getRegionWidth();
+		}
+		
+		for(Rock r: rocks) {
+			if(camera.position.x - r.position.x > 400 + r.image.getRegionWidth()) {
+				boolean isDown = MathUtils.randomBoolean();
+				r.position.x += 5 * 200;
+				r.position.y = isDown?480-rock.getRegionHeight(): 0;
+				r.image = isDown? rockDown: rock;
+			}
 		}
 	}
 	
@@ -76,8 +99,11 @@ public class PlaneGame extends ApplicationAdapter {
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		batch.draw(background, camera.position.x - background.getWidth() / 2, 0);
+		for(Rock rock: rocks) {
+			batch.draw(rock.image, rock.position.x, rock.position.y);
+		}
 		batch.draw(ground, groundOffsetX, 0);
-		batch.draw(ground, groundOffsetX + ground.getWidth(), 0);
+		batch.draw(ground, groundOffsetX + ground.getRegionWidth(), 0);
 		batch.draw(ceiling, groundOffsetX, 480 - ceiling.getRegionHeight());
 		batch.draw(ceiling, groundOffsetX + ceiling.getRegionWidth(), 480 - ceiling.getRegionHeight());
 		batch.draw(plane.getKeyFrame(planeStateTime), planePosition.x, planePosition.y);
@@ -91,5 +117,16 @@ public class PlaneGame extends ApplicationAdapter {
 		
 		updateWorld();
 		drawWorld();			
+	}
+	
+	static class Rock {
+		Vector2 position = new Vector2();
+		TextureRegion image;
+		
+		public Rock(float x, float y, TextureRegion image) {
+			this.position.x = x;
+			this.position.y = y;
+			this.image = image;
+		}
 	}
 }
