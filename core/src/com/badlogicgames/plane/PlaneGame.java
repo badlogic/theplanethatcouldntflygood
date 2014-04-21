031ee7c14b180b1f2355397dd3ccb5bb236d57f3
badlogic <contact@badlogicgames.com>
2014-04-21 18:59:07 +0200

    gravity and touch controlls

--- core/src/com/badlogicgames/plane/PlaneGame.java
+++ core/src/com/badlogicgames/plane/PlaneGame.java
@@ -6,9 +6,10 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
+import com.badlogic.gdx.math.Vector2;
 
 public class PlaneGame extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -17,7 +18,11 @@ public class PlaneGame extends ApplicationAdapter {
 	Texture ground;
 	TextureRegion ceiling;
 	Animation plane;
-	float stateTime = 0;
+	
+	Vector2 planePosition = new Vector2();
+	Vector2 planeVelocity = new Vector2();
+	float planeStateTime = 0;
+	Vector2 gravity = new Vector2();
 	
 	@Override
 	public void create () {
@@ -36,6 +41,32 @@ public class PlaneGame extends ApplicationAdapter {
 		
 		plane = new Animation(0.05f, new TextureRegion(frame1), new TextureRegion(frame2), new TextureRegion(frame3), new TextureRegion(frame2));
 		plane.setPlayMode(PlayMode.LOOP);
+		planePosition.set(50, 240);
+		planeVelocity.set(0, 0);
+		gravity.set(0, -20);
+	}
+	
+	private void updateWorld() {
+		float deltaTime = Gdx.graphics.getDeltaTime();
+		planeStateTime += deltaTime;
+		
+		if(Gdx.input.justTouched()) {
+			planeVelocity.set(0, 350);
+		}
+		
+		planeVelocity.add(gravity);
+		planePosition.mulAdd(planeVelocity, deltaTime);
+	}
+	
+	private void drawWorld() {
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		batch.draw(background, 0, 0);
+		batch.draw(ground, 0, 0);
+		batch.draw(ceiling, 0, 480 - ceiling.getRegionHeight());
+		batch.draw(plane.getKeyFrame(planeStateTime), planePosition.x, planePosition.y);
+		batch.end();
 	}
 
 	@Override
@@ -43,15 +74,7 @@ public class PlaneGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
-		camera.update();
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		batch.draw(background, 0, 0);
-		batch.draw(ground, 0, 0);
-		batch.draw(ceiling, 0, 480 - ceiling.getRegionHeight());
-		batch.draw(plane.getKeyFrame(stateTime), 50, 240);
-		batch.end();
-		
-		stateTime += Gdx.graphics.getDeltaTime();
+		updateWorld();
+		drawWorld();			
 	}
 }
